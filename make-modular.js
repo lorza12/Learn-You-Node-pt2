866f7045b23ff70e78d3520e2dab0cb62cc015b6
juan_lorza <lorza112@hotmail.com>
2022-10-31 18:44:07 -0500

    solution ex 6

--- make-modular.js
+++ make-modular.js
@@ -1,10 +1,13 @@
 const getFiles = require("./06-mymodule");
 const path = require("path");
 
+
 const folder = process.argv[2];
 const ext = process.argv[3];
 
+
 getFiles(folder, ext, function (err, list) {
+    
   if (err) {
     return console.log("There was an error", err);
   }
